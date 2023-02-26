ec2bdb56ad06c5696e7334f8fa7ef4bccb732a85
Arshcode99 <Arshcode99@gmail.com>
2023-02-26 17:58:58 +0530

    Now This.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import styles from '@/styles/Home.module.css';
-import Hero from '../public/graphics4.png';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
@@ -17,9 +15,6 @@ export default function Home() {
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem soluta ratione eum deserunt at blanditiis perspiciatis dolorem vel quisquam id numquam eligendi perferendis aspernatur quibusdam, voluptates unde commodi culpa eaque.
             </p>
             <Link href='/browse' className={styles.btn}>Get Started</Link>
-          </div>
-          <div className={styles.righthero}>
-            <Image src={Hero} alt="styles.Sorry, Image Cant Load Cuz Your Internet Connection Is Died... :)" />
           </div>
         </div>
       </div>
